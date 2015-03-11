0b4d6f5223a7e88850fd297f609015d603abb92b
MarkosAndre <lounge_act_1991@hotmail.com>
2015-03-10 22:43:29 -0700

    este es mi commit4444

--- MyApp/src/main/java/com/marcos/domain/HomeController.java
+++ MyApp/src/main/java/com/marcos/domain/HomeController.java
@@ -20,7 +20,7 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
-	 * Simply selects the home view to render by returning its name. Anotdehr commetn
+	 * Simply selects the home view to render by returning its name. Anotddehr commetn
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
